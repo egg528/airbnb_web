1edb33e34bf4f1ae57d64bc2f5090f79cab88846
egg528 <62459196+egg528@users.noreply.github.com>
2021-01-03 12:07:41 +0900

    finish searchView

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "47wjcal5cy6o(1_6j%5ka1hqoe$tp_a_mr1c68g*b=rrf+2dlk"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = []
 
 
 # Application definition
