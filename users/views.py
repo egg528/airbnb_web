42024b9b37d4ed3029699759733bd7623941f480
egg528 <62459196+egg528@users.noreply.github.com>
2021-01-05 16:05:00 +0900

    finish Sigh UP

--- users/views.py
+++ users/views.py
@@ -1,21 +1,20 @@
-from django.views import View
 from django.views.generic import FormView
 from django.urls import reverse_lazy
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import redirect, reverse
 from django.contrib.auth import authenticate, login, logout
-from . import forms, models
+from . import forms
 
 
 class LoginView(FormView):
+
     template_name = "users/login.html"
     form_class = forms.LoginForm
     success_url = reverse_lazy("core:home")
 
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
-        user = models.User.objects.get(email=email)
         password = form.cleaned_data.get("password")
-        user = authenticate(self.request, username=user, password=password)
+        user = authenticate(self.request, username=email, password=password)
         if user is not None:
             login(self.request, user)
         return super().form_valid(form)
@@ -24,3 +23,20 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {"first_name": "Nicoas", "last_name": "Serr", "email": "itn@las.com"}
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
