e6af587a116b896e432be4ae7a8e6bc247d24604
egg528 <62459196+egg528@users.noreply.github.com>
2021-01-09 21:28:09 +0900

    decorate website by tailwind

--- rooms/models.py
+++ rooms/models.py
@@ -119,3 +119,7 @@ class Room(core_models.TimeStampedModel):
         return round(all_ratings / len(all_reviews), 2)
 
     # related_name => room에서 user을 외래키로 사용지만 user에서도 room_set을 이용해 user의 room정보를 얻을 수 있고 이때 room_set 대신 related_name을 통해 정보를 얻을 수 있다.)
+
+    def first_photo(self):
+        (photo,) = self.photos.all()[:1]
+        return photo.file.url
